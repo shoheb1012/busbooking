4f13c05a82d8473a78da77430bbdd3383e004914
shoheb1012 <shoheb.pathan@atyeti.com>
2025-07-08 14:49:49 +0530

    Done with all end-points

--- src/main/java/com/quickbus/busbooking/BusBookingSystemApplication.java
+++ src/main/java/com/quickbus/busbooking/BusBookingSystemApplication.java
@@ -1,6 +1,5 @@
 package com.quickbus.busbooking;
 
-import com.quickbus.busbooking.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
